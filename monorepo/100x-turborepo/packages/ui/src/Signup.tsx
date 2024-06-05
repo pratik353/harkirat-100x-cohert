2183323ea18ddc400a64ed1c9574f77d06db35d6
Turbobot <turbobot@vercel.com>
2024-06-05 18:54:54 +0530

    feat(create-turbo): apply official-starter transform

---
+++ monorepo/100x-turborepo/packages/ui/src/Signup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Signup = () => {
+  return (
+    <div>
+      <input type='text' placeholder='email'/>
+      <input type='password' placeholder='password'/>
+      <button>Submit</button>
+    </div>
+  )
+}
+
